46e8d452ebaf8860d779035b8f961d17c9aeecd5
Cvalap <56564318+Cvalap@users.noreply.github.com>
2019-12-05 16:43:19 -0600

    Solucion
    Solución de tarea semana 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a "reverse array" object that you can cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    I = NULL
+    set <- function(y) {
+        x <<- y
+        I <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) I <<- inverse
+    getinverse <- function() I
+    list(set = set, 
+         get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse Matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    I <- x$getinverse()
+    if(!is.null(I)){
+        message("getting cached data")
+        return(I)
+    }
+    data <- x$get()
+    I <- solve(data,...)
+    x$setinverse(I)
+    I      
 }
+
+
+#Test functions
+m <- matrix(1:4,2,2)
+minv <- makeCacheMatrix(m)
+w <- cacheSolve(minv)
